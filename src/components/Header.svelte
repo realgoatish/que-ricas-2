29109d0eb0ff50200439a8dffdffea7aae0e86f6
Michael Lamb <mflambjr@gmail.com>
2021-01-25 18:20:06 -0500

    add order online link beneath social icons

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,22 +1,46 @@
 <script>
-
-  import ImageLoader from './images/ImageLoader.svelte'
-  import Nav from './Nav.svelte'
-  import Social from './Social.svelte'
+  import ImageLoader from "./images/ImageLoader.svelte";
+  import Nav from "./Nav.svelte";
+  import Social from "./Social.svelte";
 
   // Declare props to "catch" properties of the prop object we passed in via _layout.svelte (parent component)
-  export let logo
-  export let socialIcons
-  export let contactInfo
-  export let businessHours
+  export let logo;
+  export let socialIcons;
+  export let orderOnline;
+  export let contactInfo;
+  export let businessHours;
   // export let nav
 
-  $: console.log(businessHours)
-
+  // $: console.log(businessHours);
 </script>
 
-<style>
+<header class="section-wrapper">
+  <section class="container">
+    <section class="header__logo-social-contact">
+      <ImageLoader src={logo.image} alt={logo.altText} />
+      <Social {socialIcons} />
+      <section class="header__contact-hours">
+        <p>
+          <a class="link__order-online" href={orderOnline.link}
+            >{orderOnline.visibleText}</a
+          >
+        </p>
+        <p>{contactInfo.phoneNumber}</p>
+        <p>{contactInfo.streetAddress},</p>
+        <p>{contactInfo.city}, {contactInfo.state} {contactInfo.zip}</p>
+        {#each businessHours as item}
+          <p>{item}</p>
+        {/each}
+      </section>
+    </section>
 
+    <section class="header-nav">
+      <Nav />
+    </section>
+  </section>
+</header>
+
+<style>
   header {
     background: #fff;
     border-bottom-style: outset;
@@ -36,48 +60,14 @@
     /* max-width:  */
   }
 
-  /* :global(header p) {
-    margin: 0;
-    text-align: center;
+  .link__order-online {
+    color: #3271ab;
+    text-decoration: underline;
   }
 
-  :global(header p, header a) {
-    font-size: 85%;
-  } */
-
   @media (min-width: 400px) {
-
     .header__logo-social-contact {
       width: 14rem;
     }
-
-    /* :global(header p, header a) {
-      font-size: 100%;
-    } */
   }
-
 </style>
-
-<header class="section-wrapper">
-  <section class="container">
-    <section class="header__logo-social-contact">
-      <ImageLoader src={logo.image} alt={logo.altText}/>
-      <Social {socialIcons}/>
-      <section class="header__contact-hours">
-        <p>{contactInfo.phoneNumber}</p>
-        <p>{contactInfo.streetAddress},</p>
-        <p>{contactInfo.city}, {contactInfo.state} {contactInfo.zip}</p>
-        {#each businessHours as item}
-          <p>{item}</p>
-        {/each}
-      </section>
-
-
-    </section>
-
-    <section class="header-nav">
-      <Nav />
-    </section>
-  </section>
-</header>
-
