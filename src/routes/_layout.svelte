53fb2ab29f02060743376475390fdc90e68967ba
Michael Lamb <mflambjr@gmail.com>
2021-01-26 11:34:50 -0500

    use svelte:head & check routes to load data for w/e page

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,11 +1,26 @@
 <script>
   // export let segment
 
+  import { stores } from "@sapper/app";
+  const { page } = stores();
+
+  $: console.log($page);
+
   import { layout } from "../transformers";
   const [layoutContent] = layout;
 
   import Header from "./../components/Header.svelte";
 </script>
+
+<svelte:head>
+  {#if $page.path === "/"}
+    <title>Home</title>
+  {:else if $page.path === "/menu"}
+    <title>Menu</title>
+  {:else}
+    <title>About</title>
+  {/if}
+</svelte:head>
 
 <main>
   <Header {...layoutContent} />
