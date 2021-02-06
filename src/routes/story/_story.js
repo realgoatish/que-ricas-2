eb0d2e657f8ede8fa77566c99fa9ea823070a258
Michael Lamb <mflambjr@gmail.com>
2021-02-06 17:37:32 -0500

    about page

---
+++ src/routes/story/_story.js
@@ -0,0 +1,7 @@
+import story from './../../../markdowns/story/*.md'
+
+
+
+export const aboutPage = story
+  .map(({ metadata, html }) => ({ ...metadata, html }))
+
---
+++ src/routes/story/index.svelte
@@ -0,0 +1,88 @@
+<script context="module">
+  export async function preload({ params, query }) {
+    return this.fetch(`story.json`)
+      .then((r) => r.json())
+      .then((story) => {
+        return { story };
+      });
+  }
+</script>
+
+<script>
+  import { stores } from "@sapper/app";
+  import BaseSEO from "../../components/BaseSEO.svelte";
+  import ImageLoader from "./../../components/images/ImageLoader.svelte";
+  import Card from "./../../components/Card.svelte";
+
+  export let story;
+
+  // $: console.log(story);
+
+  const [darStory, saharStory] = story;
+
+  const { page } = stores();
+</script>
+
+<BaseSEO
+  title="Our Story"
+  description="Sahar loves bringing you the best South American food in Haddon Township at Que Ricas"
+  canonical={`https://${$page.host}${$page.path}`}
+  image={saharStory.image}
+  altText={saharStory.altText}
+/>
+
+<article class="page-content">
+  <Card>
+    <section>
+      <figure class="image__article sahar">
+        <ImageLoader
+          src={saharStory.image}
+          alt={saharStory.altText}
+          rounded={true}
+        />
+      </figure>
+      <div class="text__article">
+        {@html saharStory.html}
+      </div>
+    </section>
+  </Card>
+
+  <Card>
+    <section>
+      <figure class="image__article dar">
+        <ImageLoader
+          src={darStory.image}
+          alt={darStory.altText}
+          rounded={true}
+        />
+      </figure>
+      <div class="text__article">
+        {@html darStory.html}
+      </div>
+    </section>
+  </Card>
+</article>
+
+<style>
+  section {
+    overflow: hidden;
+  }
+  .image__article {
+    max-width: 40%;
+    overflow: hidden;
+  }
+
+  .sahar {
+    float: left;
+    margin: 0 20px 15px 0;
+  }
+
+  .dar {
+    float: right;
+    margin: 0 0 15px 20px;
+  }
+
+  .text__article {
+    line-height: 1.75;
+  }
+</style>
