993c12e3010a145387284de3f0780f71398b99a3
Michael Lamb <mflambjr@gmail.com>
2021-02-10 17:11:11 -0500

    clearing stashes

--- src/routes/_home.js
+++ src/routes/_home.js
@@ -5,8 +5,8 @@ const fs = require('fs')
 export const homePage = home
   .map(({ metadata, html }) => {
     if (metadata.foodImage && metadata.hdImage) {
-      metadata.foodImage = metadata.foodImage.replace('.', 'https://www.que-ricas.com')
-      metadata.hdImage = metadata.hdImage.replace('.', 'https://www.que-ricas.com')
+      metadata.foodImage = metadata.foodImage.replace('.', 'https://www.que-ricas.com/g')
+      metadata.hdImage = metadata.hdImage.replace('.', 'https://www.que-ricas.com/g')
     }
     return ({ ...metadata, html })
   })
