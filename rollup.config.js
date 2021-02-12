fc76391d386cbac28f97e9fba3f4e4c7c89d12e2
Michael Lamb <mflambjr@gmail.com>
2021-02-11 21:36:24 -0500

    delete commented-out service worker code

--- rollup.config.js
+++ rollup.config.js
@@ -134,20 +134,4 @@ export default {
 		onwarn,
 	},
 
-	// serviceworker: !dev && {
-	// 	input: config.serviceworker.input(),
-	// 	output: config.serviceworker.output(),
-	// 	plugins: [
-	// 		resolve(),
-	// 		replace({
-	// 			'process.browser': true,
-	// 			'process.env.NODE_ENV': JSON.stringify(mode)
-	// 		}),
-	// 		commonjs(),
-	// 		!dev && terser()
-	// 	],
-
-	// 	preserveEntrySignatures: false,
-	// 	onwarn,
-	// }
 };
