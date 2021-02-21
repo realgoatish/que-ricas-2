a6948db067189d2ff684e32775a87a3c283120c5
Michael Lamb <mflambjr@gmail.com>
2021-02-21 02:34:24 -0500

    try 600x600 og:image

--- src/components/BaseSEO.svelte
+++ src/components/BaseSEO.svelte
@@ -197,8 +197,8 @@
   <meta property="og:title" content={title} />
   <meta property="og:image" content={image} />
   <meta property="og:image:alt" content={altText} />
-  <meta property="og:image:width" content="1024" />
-  <meta property="og:image:height" content="1024" />
+  <meta property="og:image:width" content="600" />
+  <meta property="og:image:height" content="600" />
   <meta property="og:type" content="website" />
   <meta property="og:url" content={canonical} />
   {@html jsonldScript}
