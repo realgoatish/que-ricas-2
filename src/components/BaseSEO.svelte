ed51baf174d0cc6f2ddd7f6a0d1226b0b7c372a7
Michael Lamb <mflambjr@gmail.com>
2021-02-21 02:23:46 -0500

    adjust og:image:width & height for square images

--- src/components/BaseSEO.svelte
+++ src/components/BaseSEO.svelte
@@ -196,8 +196,8 @@
   <meta property="og:title" content={title} />
   <meta property="og:image" content={image} />
   <meta property="og:image:alt" content={altText} />
-  <meta property="og:image:width" content="1200" />
-  <meta property="og:image:height" content="630" />
+  <meta property="og:image:width" content="1024" />
+  <meta property="og:image:height" content="1024" />
   <meta property="og:type" content="website" />
   <meta property="og:url" content={canonical} />
   {@html jsonldScript}
