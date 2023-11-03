401c9a673870373ca65136a47987562408616fcd
Mike Lamb <mflambjr@gmail.com>
2023-11-02 23:45:03 -0400

    add order online landing page

---
+++ markdowns/orderOnline/page.md
@@ -0,0 +1,11 @@
+---
+title: Order Online
+options:
+  pickup: 
+    text: "Order Pickup"
+    link: "https://www.clover.com/online-ordering/que-ricas"
+  delivery1: 
+    text: "Order GrubHub"
+    link: "https://www.grubhub.com/restaurant/que-ricas-46-haddon-ave-haddon-township/6837624"
+
+---
---
+++ src/routes/order-online/index.svelte
@@ -0,0 +1,96 @@
+<script context="module">
+  export async function preload({ params, query }) {
+    return this.fetch(`order-online.json`)
+      .then((r) => r.json())
+      .then((page) => {
+        return { page };
+      });
+  }
+</script>
+
+<script>
+  import Card from "./../../components/Card.svelte";
+
+  export let page;
+
+  const [metadata] = page;
+</script>
+
+<article class="page-content">
+  <!-- {JSON.stringify(page, null, 2)} -->
+  <Card>
+    <div class="center">
+      <div class="stack">
+        <div>
+          <h1>{metadata.title}</h1>
+        </div>
+        <div class="cluster">
+          <a href={metadata.options.pickup.link}
+            >{metadata.options.pickup.text}</a
+          >
+          <a href={metadata.options.delivery1.link}
+            >{metadata.options.delivery1.text}</a
+          >
+        </div>
+      </div>
+    </div>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+  </Card>
+</article>
+
+<style>
+  a {
+    text-decoration: none;
+    border: 1px solid rgb(245, 124, 0);
+    /* width: 7rem; */
+    color: #225a8c;
+    padding: 0.6rem 0.4rem;
+    margin-top: 0.7rem;
+    border-radius: 2px;
+    background-color: #fff;
+    transition: 0.3s cubic-bezier(0.25, 0.8, 0.5, 1) background-color 1ms;
+  }
+
+  a:hover {
+    background-color: #fef0e8;
+  }
+
+  div {
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  /* .center {
+    box-sizing: content-box;
+    margin-inline: auto;
+  } */
+
+  .stack {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+  }
+
+  .stack > * {
+    margin-block: 0;
+  }
+
+  .stack > * + * {
+    margin-block-start: 2rem;
+  }
+
+  .cluster {
+    display: flex;
+    flex-wrap: wrap;
+    gap: var(--space, 1.5rem);
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
