d4d58ed902a117171ce2b8bdce80a5f5d67e8875
Mike Lamb <mflambjr@gmail.com>
2025-05-09 21:58:15 -0400

    tell sitemap not to look for images in order-online dir

--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -1,52 +1,79 @@
-const fs = require('fs')
+const fs = require("fs");
 // const probe = require('probe-image-size')
 
-const BASE_URL = "https://www.que-ricas.com"
-const pages = [""]
+const BASE_URL = "https://www.que-ricas.com";
+const pages = [""];
 
-fs.readdirSync("./src/routes").forEach(file => {
-  file = file.split('.')[0]
-  if (file.charAt(0) !== '_' && file !== "sitemap" && file !== "index" && file !== "layout") {
-    pages.push(file)
+fs.readdirSync("./src/routes").forEach((file) => {
+  file = file.split(".")[0];
+  if (
+    file.charAt(0) !== "_" &&
+    file !== "sitemap" &&
+    file !== "index" &&
+    file !== "layout"
+  ) {
+    pages.push(file);
   }
-})
+});
 
+// TODO - may delete this since it checks all dirs & order-online page has no images on it, not even an og:image
+// const getPageImages = (imageDir) => {
+//   let result = [];
+//   if (imageDir === "") {
+//     imageDir = "home";
+//   }
 
-const getPageImages = (imageDir => {
-  let result = []
-  if (imageDir === '') {
-    imageDir = 'home'
+//   fs.readdirSync(`./static/images/${imageDir}`).forEach((file) => {
+//     result.push(file);
+//   });
+//   return result;
+// };
+
+const getPageImages = (imageDir) => {
+  let result = [];
+  if (imageDir === "") {
+    imageDir = "home";
   }
-  fs.readdirSync(`./static/images/${imageDir}`).forEach(file => {
-    result.push(file)
-  })
-  return result
-})
+
+  // filter out order-online dir since it has no images on page or og:image
+  if (imageDir !== "order-online") {
+    fs.readdirSync(`./static/images/${imageDir}`).forEach((file) => {
+      result.push(file);
+    });
+  }
+
+  return result;
+};
 
 // let data = fs.readFileSync('./static/images/global/que-ricas-logo.png');
 // console.log(probe.sync(data));
 
-
 const render = (pages) => `<?xml version="1.0" encoding="UTF-8" ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">${pages
   .map(
-    page => `
+    (page) => `
   <url>
-    <loc>${BASE_URL}${page === '' ? page : '/' + page}</loc>
+    <loc>${BASE_URL}${page === "" ? page : "/" + page}</loc>
     <priority>0.85</priority>${getPageImages(page)
-      .map(img => `
+      .map(
+        (img) => `
     <image:image>
-      <image:loc>${BASE_URL}/images/${page === '' ? 'home' : page}/${img}</image:loc>
-    </image:image>`).join("")}
-  </url>`).join("")}
+      <image:loc>${BASE_URL}/images/${
+          page === "" ? "home" : page
+        }/${img}</image:loc>
+    </image:image>`
+      )
+      .join("")}
+  </url>`
+  )
+  .join("")}
 </urlset>
-  `
+  `;
 
 export function get(req, res) {
-
   res.writeHead(200, {
-    'Cache-Control': `max-age=0, s-max-age=${600}`,
-    'Content-Type': 'application/rss+xml'
+    "Cache-Control": `max-age=0, s-max-age=${600}`,
+    "Content-Type": "application/rss+xml",
   });
 
   const sitemap = render(pages);
